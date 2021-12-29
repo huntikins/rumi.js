0e4697c4314913e518e53ccc7918d7476dc5f376
Hunter Trammell <hunterttrammell@gmail.com>
2021-12-29 11:39:25 -0600

    [feature] Added in page templates

---
+++ components/card/GameCard.tsx
@@ -0,0 +1,16 @@
+function GameCard(){
+    return (
+        <article className="room">
+            <div className="room-data">
+                <div className="room-data_title">
+                    <h3>Room Name</h3>
+                </div>
+                <div className="room-data_info">
+                    Room Info
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default GameCard;
